61706becd99a2a09b5ee6ae45466b6aaa37c486d
thejas <thejaskala308@gmail.com>
2022-09-01 12:53:15 +0530

    make it better

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Header from '../components/Header'
 import {sanityClient, urlFor} from '../sanity'
 import {Post} from '../typings'
